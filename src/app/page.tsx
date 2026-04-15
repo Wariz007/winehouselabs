de9907538844b7d45b7a0d06310e5e925bf85f6c
wariz007 <bookbodmas31@gmail.com>
2026-04-15 09:43:45 +0100

    90% done with about us and community section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero/Hero";
 import Resources from "@/components/ResourcesPage/Resource";
+import AboutUsAndCommunity from "@/components/About-us-and-Community/AandC";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Resources />
+      <AboutUsAndCommunity />
     </main>
   );
 }
---
+++ src/components/About-us-and-Community/AandC.tsx
@@ -0,0 +1,34 @@
+import styles from './AandC.module.scss';
+import Image from 'next/image';
+
+export default function AboutUsAndCommunity() {
+    return (
+        <section className={styles.aboutUsAndCommunity}>
+            <div className={styles.aboutUs}>
+                <p>
+                    Our ultimate goal is to make sure our actions contribute to the prosperity and pride of Nigeria. Using the knowledge we accumulate, we aim to solve real-world problems and create meaningful change across Nigeria—from local communities to the national stage
+                </p>
+
+                <button>Read more about us</button>
+            </div>
+
+            <div className={styles.community}>
+                <Image 
+                    src="/WHL-community-design.png"
+                    alt='WHL-community-design.png'
+                    width={350}
+                    height={200}
+                />
+
+                <div className={styles.communityText}>
+                    <p className={styles.heading}>There is so much to build. <br /> And fix</p>
+                    <p>
+                        The Winehouse community is focused on building generations of capable Nigerian software engineers who will rise to the challenge of solving Nigeria's toughest problems
+                    </p>
+                    <button>Join Us!</button>
+                </div>
+            </div>
+
+        </section>
+    )
+}
