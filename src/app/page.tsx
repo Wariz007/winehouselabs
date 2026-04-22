212e2d190c0a70f905fe6df21c514aa3c2fb9f00
wariz007 <bookbodmas31@gmail.com>
2026-04-22 19:36:25 +0100

    feat: connected supabase to next.js project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Hero from "@/components/Hero/Hero";
 import Resources from "@/components/ResourcesPage/Resource";
 import AboutUsAndCommunity from "@/components/About-us-and-Community/AandC";
 
-export default function Home() {
+export default async function Home() {
+
   return (
     <main>
       <Hero />
